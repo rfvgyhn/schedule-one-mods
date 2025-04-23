623ba514c6b90266f5c9eec4fdf3034a1cd00128
Chris <github@nsaas.io>
2025-04-23 15:10:11 -0600

    use zip instead of tar for windows users

--- publish.sh
+++ publish.sh
@@ -3,7 +3,8 @@
 target=win-x64
 version=$(grep -oPm1 "(?<=<VersionPrefix>)[^<]+" src/Directory.Build.props)
 release_name="schedule-one-mods_v${version}"
-release_path="artifacts/$release_name"
+artifacts_path=artifacts
+release_path="$artifacts_path/$release_name"
 
 dotnet restore -r $target
 dotnet publish src/ScheduleOneMods.DealsSummary/ScheduleOneMods.DealsSummary.csproj -r $target --no-restore -o "$release_path" -c Release
@@ -12,6 +13,8 @@ rename -v ScheduleOneMods Rfvgyhn "$release_path"/*
 
 cp README.md CHANGELOG.md "$release_path"
 
-tar czvf "$release_path.tar.gz" -C "artifacts" "$release_name"
+pushd $artifacts_path
+rm -f "$release_name.zip" && zip -r "$release_name.zip" "$release_name"
+popd
 
 rm -r "$release_path"
