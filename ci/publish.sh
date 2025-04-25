2d0430fcc8d5ee2b4663922f3b966c6fde62a5cd
Chris <github@nsaas.io>
2025-04-25 14:49:35 -0600

    [ci] use built in assembly renaming

--- ci/publish.sh
+++ ci/publish.sh
@@ -2,25 +2,12 @@
 
 set -e
 
-for cmd in perl-rename rename; do
-    if command -v "$cmd" >/dev/null; then
-        RENAME_CMD="$cmd"
-        break
-    fi
-done
-
-if [ -z "$RENAME_CMD" ]; then
-    echo "Error: Neither 'rename' nor 'perl-rename' was found" >&2
-    exit 1
-fi
-
 target="$1"
 release_name="$2"
 config="$3"
 version_suffix="$4"
 release_path="artifacts/$release_name"
 
-dotnet publish src/ScheduleOneMods.DealsSummary/ScheduleOneMods.DealsSummary.csproj -r "$target" --no-restore -o "$release_path" -c "$config" -p:VersionSuffix="$version_suffix"
-dotnet publish src/ScheduleOneMods.CounterPriceButton/ScheduleOneMods.CounterPriceButton.csproj -r "$target" --no-restore -o "$release_path" -c "$config" -p:VersionSuffix="$version_suffix"
-find "$release_path"/ ! -name 'ScheduleOneMods.*.dll' -type f -delete
-$RENAME_CMD 's/ScheduleOneMods/Rfvgyhn/' "$release_path"/*
+dotnet publish src/ScheduleOneMods.DealsSummary/ScheduleOneMods.DealsSummary.csproj -r "$target" --no-restore -o "$release_path" -c "$config" -p:VersionSuffix="$version_suffix" -p:AssemblyName="Rfvgyhn.DealsSummary"
+dotnet publish src/ScheduleOneMods.CounterPriceButton/ScheduleOneMods.CounterPriceButton.csproj -r "$target" --no-restore -o "$release_path" -c "$config" -p:VersionSuffix="$version_suffix" -p:AssemblyName="Rfvgyhn.CounterPriceButton"
+find "$release_path"/ ! -name 'Rfvgyhn.*.dll' -type f -delete
--- publish.sh
+++ publish.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-for cmd in perl-rename rename; do
-    if command -v "$cmd" >/dev/null; then
-        RENAME_CMD="$cmd"
-        break
-    fi
-done
-
-if [ -z "$RENAME_CMD" ]; then
-    echo "Error: Neither 'rename' nor 'perl-rename' was found" >&2
-    exit 1
-fi
-
 target=win-x64
 version=$(grep -oPm1 "(?<=<VersionPrefix>)[^<]+" src/Directory.Build.props)
 release_name="schedule-one-mods_v${version}"
@@ -21,10 +9,9 @@ artifacts_path=artifacts
 release_path="$artifacts_path/$release_name"
 
 dotnet restore -r $target
-dotnet publish src/ScheduleOneMods.DealsSummary/ScheduleOneMods.DealsSummary.csproj -r $target --no-restore -o "$release_path" -c Release
-dotnet publish src/ScheduleOneMods.CounterPriceButton/ScheduleOneMods.CounterPriceButton.csproj -r $target --no-restore -o "$release_path" -c Release
-find "$release_path"/ ! -name 'ScheduleOneMods.*.dll' -type f -delete
-$RENAME_CMD 's/ScheduleOneMods/Rfvgyhn/' "$release_path"/*
+dotnet publish src/ScheduleOneMods.DealsSummary/ScheduleOneMods.DealsSummary.csproj -r $target --no-restore -o "$release_path" -c Release -p:AssemblyName="Rfvgyhn.DealsSummary"
+dotnet publish src/ScheduleOneMods.CounterPriceButton/ScheduleOneMods.CounterPriceButton.csproj -r $target --no-restore -o "$release_path" -c Release -p:AssemblyName="Rfvgyhn.CounterPriceButton"
+find "$release_path"/ ! -name 'Rfvgyhn.*.dll' -type f -delete
 
 cp README.md CHANGELOG.md "$release_path"
 
