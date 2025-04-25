120e33c7a21f21c1db485d0c82313ba1d79812ef
Chris <github@nsaas.io>
2025-04-24 18:42:17 -0600

    [ci] properly quote perl strings for string mode

--- ci/publish.sh
+++ ci/publish.sh
@@ -9,4 +9,4 @@ release_path="artifacts/$release_name"
 dotnet publish src/ScheduleOneMods.DealsSummary/ScheduleOneMods.DealsSummary.csproj -r "$target" --no-restore -o "$release_path" -c "$config" -p:VersionSuffix="$version_suffix"
 dotnet publish src/ScheduleOneMods.CounterPriceButton/ScheduleOneMods.CounterPriceButton.csproj -r "$target" --no-restore -o "$release_path" -c "$config" -p:VersionSuffix="$version_suffix"
 find "$release_path"/ ! -name 'ScheduleOneMods.*.dll' -delete
-rename -v ScheduleOneMods Rfvgyhn "$release_path"/*
+rename -v 'ScheduleOneMods' 'Rfvgyhn' "$release_path"/*
