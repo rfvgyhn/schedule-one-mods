d11ede0e64ab16519fae6b5c62c5813f8bb65641
Chris <github@nsaas.io>
2025-04-26 16:03:01 -0600

    [ci] show descriptive output when downloading game assemblies

--- ci/download-game-assemblies.sh
+++ ci/download-game-assemblies.sh
@@ -5,19 +5,26 @@ destination="$(dirname "$(readlink -f "$0")")/../lib"
 github_url="https://api.github.com/repos/${GITHUB_REPO}/releases"
 
 #https://docs.github.com/en/rest/releases/releases#get-the-latest-release
-asset_id=$(
-    curl -L \
+release=$(
+    curl -L -s \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer ${GITHUB_TOKEN}" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
-      "$github_url/latest" \
-      | jq -r '.assets[0] .id'
+      "$github_url/latest"
 )
+description=$(echo "$release" | jq -r '.body')
+asset_id=$(echo  | jq -r '.assets[0] .id')
 
 # https://docs.github.com/en/rest/reference/repos#get-a-release-asset
-curl -L \
+curl -L -s \
      -H "Accept: application/octet-stream" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
      -H "X-GitHub-Api-Version: 2022-11-28" \
         "$github_url/assets/${asset_id}" \
-     | tar --overwrite --verbose -xzf - -C "$destination"
+     | tar --overwrite -xzf - -C "$destination"
+
+echo "$description"
+echo "sha256"
+pushd "$destination" > /dev/null
+    find . -type f -name "*.dll" -printf '%P\n' | xargs shasum -a 256 -b | sort
+popd > /dev/null
