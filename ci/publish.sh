b861aa221dea51a4fd4fe2150e6e442290084869
Chris <github@nsaas.io>
2025-04-25 22:26:18 -0600

    [ci] fix incorrect param when publishing project

--- ci/publish.sh
+++ ci/publish.sh
@@ -9,5 +9,5 @@ config="$4"
 version_suffix="$5"
 release_path="artifacts/$release_name"
 
-dotnet publish "src/ScheduleOneMods.$project/ScheduleOneMods.$project.csproj" -r "$target" --no-restore --no-build -o "$release_path" -c "$config" -p:VersionSuffix="$version_suffix" -p:AssemblyName="Rfvgyhn.$project"
+dotnet publish "src/ScheduleOneMods.$project/ScheduleOneMods.$project.csproj" -r "$target" --no-restore -o "$release_path" -c "$config" -p:VersionSuffix="$version_suffix" -p:AssemblyName="Rfvgyhn.$project"
 find "$release_path"/ ! -name 'Rfvgyhn.*.dll' -type f -delete
